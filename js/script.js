a78a4b76f24688ced9f1aed9cfe18de82b8a28c1
Akhazogie Victoria <akhazogievictoria2@gmail.com>
2023-02-02 12:12:37 +0100

    add changes to script

--- js/script.js
+++ js/script.js
@@ -5,33 +5,55 @@ $(document).ready(function () {
     const a = parseInt($("#a").val());
     const b = parseInt($("#b").val());
     const c = parseInt($("#c").val());
-    console.log(a)
-    console.log(b)
-    console.log(c)
 
-    if (a === b && b === c) {
-      $(".equi").show()
-      $(".iso").hide()
-      $(".scal").hide()
-      $(".not").hide()
-    }
-    else if ((a === b || b === c || c == a) && (a + b > c || a + c > b || b + c > a)) {
-      $(".iso").show()
-      $(".scal").hide()
-      $(".not").hide()
-      $(".equi").hide()
-    }
-    else if (a != b && b != c && a != c && (a + b > c || a + c > b || b + c > a)){
-      $(".scal").show()
-      $(".iso").hide()
-      $(".not").hide()
-      $(".equi").hide()
-    }
-    else if(a + b <= c || b + c <= a || c + a <= b){
+
+    if (
+      (a + b <= c || b + c <= a || c + a <= b) ||
+      (a <= 0 || b <= 0 || c <= 0)
+      ) {
       $(".not").show()
       $(".iso").hide()
       $(".scal").hide()
       $(".equi").hide()
+      $("#form1").hide()
+      $(".up").hide()
+    }
+    else if (
+      (a === b && b === c) 
+      ) {
+      $(".equi").show()
+      $(".iso").hide()
+      $(".scal").hide()
+      $(".not").hide()
+      $("#form1").hide()
+      $(".up").hide()
+
+    }
+    else if (
+      ((a === b) && (a + b > c)) ||
+      ((b === c) && (b + c > a)) ||
+      ((c === a) && (c + a > b)) 
+
+    ) {
+      $(".iso").show()
+      $(".scal").hide()
+      $(".not").hide()
+      $(".equi").hide()
+      $("#form1").hide()
+      $(".up").hide()
+
+    }
+    else if (
+      (a != b && b != c && a != c) &&
+      (a + b > c || a + c > b || b + c > a) 
+      ) {
+      $(".scal").show()
+      $(".iso").hide()
+      $(".not").hide()
+      $(".equi").hide()
+      $("#form1").hide()
+      $(".up").hide()
+
     }
   });
 });
